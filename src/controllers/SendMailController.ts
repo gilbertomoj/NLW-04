eb07afe84e6715d23b4340aebd2937bb88835ce1
ggibamede <gibamedeiros.gmail.com>
2021-02-26 12:17:43 -0300

    feature(func): Refatorando o arquivo SurveyController,ts

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -26,21 +26,25 @@ class SendMailController{
         if(!survey){
             return response.status(400).json({error:"Survey does not exists"});
         }
-        const variables = {
-            name: user.name,
-            title : survey.title,
-            description: survey.description,
-            user_id: user.id,
-            link: process.env.URL_MAIL,
-        }
+       
         const npsPath = resolve(__dirname,"..","views","emails","npsMail.hbs");
 
 
         const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{user_id: user.id}, {value: null}],
+            where: {user_id: user.id , value: null},
             relations: ["user","survey"]
         });
+
+        const variables = {
+            name: user.name,
+            title : survey.title,
+            description: survey.description,
+            id: "",
+            link: process.env.URL_MAIL,
+        }
+
         if(surveyUserAlreadyExists){
+            variables.id = surveyUserAlreadyExists.id; 
             await SendMailService.execute(email, survey.title, variables, npsPath)
             return response.json(surveyUserAlreadyExists)
         }
@@ -55,6 +59,7 @@ class SendMailController{
 
         await surveysUsersRepository.save(surveyUser);
         
+        variables.id = surveyUser.id;
 
         await SendMailService.execute(email, survey.title , variables, npsPath);
 
